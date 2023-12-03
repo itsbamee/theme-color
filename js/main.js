ab8c1bfe186abd932c4041077b6fbab2634f9d20
itsbamee <jan9239@naver.com>
2023-12-03 10:38:09 +0900

    버튼 클릭시 클릭한 색상값을 쿠키에 등록해서 body의 클래스로 지정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 const colors = ['lightblue', 'pink', 'lightgreen', '#333'];
-
 const [btnColors, btnReset] = createPickerDom(colors);
 
+console.log(document.cookie);
+
+//처음 로딩시 color=으로 시작하는 쿠키가 없으면 강제로 body요소에 클래스명을 제거
+if (document.cookie.indexOf('color=') < 0) {
+	document.body.className = '';
+} else {
+	//만약에 로딩시 color=으로 시작하는 쿠기가 있으면 해당 쿠키 순번에서 color= 다음에 있는 색상 문자값만 잘라내기해서 body의 클래스로 지정
+	const pos = document.cookie.indexOf('color=');
+	const restCookie = document.cookie.slice(pos + 6);
+	const colorVal = restCookie.split(' ')[0];
+	document.body.className = colorVal;
+}
+
+//span버튼 클릭 시 해당 색상값으로 cookie='색상값' 형태의 쿠키 생성
 btnColors.forEach((el, idx) => {
 	el.addEventListener('click', (e) => {
 		const color = e.currentTarget.innerText;
 		console.log(color);
+		setCookie('color', color, 1);
+		document.body.className = color;
 	});
+});
+
+btnReset.addEventListener('click', () => {
+	const pos = document.cookie.indexOf('color=');
+	if (pos < 0) {
+		const restCookie = document.cookie.slice(pos + 6);
+		const colorVal = restCookie.split(' ')[0];
+		setCookie('color', colorVal, 0);
+	}
 });
 
 function createPickerDom(arr) {
