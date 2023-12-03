b73f304dc4cf2d232d681572525c4d3370702e0b
itsbamee <jan9239@naver.com>
2023-12-03 11:29:22 +0900

    자스로 이벤트 발생시 reset에 등록된 특정 변수 스타일값 재할당

--- js/main.js
+++ js/main.js
@@ -3,24 +3,23 @@ const [btnColors, btnReset] = createPickerDom(colors);
 
 console.log(document.cookie);
 
-//처음 로딩시 color=으로 시작하는 쿠키가 없으면 강제로 body요소에 클래스명을 제거
 if (document.cookie.indexOf('color=') < 0) {
 	document.body.className = '';
 } else {
-	//만약에 로딩시 color=으로 시작하는 쿠기가 있으면 해당 쿠키 순번에서 color= 다음에 있는 색상 문자값만 잘라내기해서 body의 클래스로 지정
 	const pos = document.cookie.indexOf('color=');
 	const restCookie = document.cookie.slice(pos + 6);
 	const colorVal = restCookie.split(' ')[0];
-	document.body.className = colorVal;
+	document.body.className = 'theme_' + colorVal;
 }
 
-//span버튼 클릭 시 해당 색상값으로 cookie='색상값' 형태의 쿠키 생성
 btnColors.forEach((el, idx) => {
 	el.addEventListener('click', (e) => {
 		const color = e.currentTarget.innerText;
 		console.log(color);
 		setCookie('color', color, 1);
-		document.body.className = color;
+		//getcomputedStyle(document.body).getPropertyValue('--pointColor');
+		document.body.style.setProperty('--pointColor', color);
+		document.body.className = 'theme_' + color;
 	});
 });
 
